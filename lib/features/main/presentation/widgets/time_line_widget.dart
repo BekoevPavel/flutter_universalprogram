aa18e8fdc5ec48881bdd55b21e4b3861e34dd5fe
Pavel Bekoev <pablank@bk.ru>
2022-01-31 05:02:46 +0300

    addTimeline1

--- lib/features/main/presentation/widgets/time_line_widget.dart
+++ lib/features/main/presentation/widgets/time_line_widget.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_universalprogram/features/main/domain/all_data.dart';
+import 'package:flutter_universalprogram/features/main/domain/sourse/line_container_sourse.dart';
+
 class TimeLineWidget extends StatelessWidget {
-  const TimeLineWidget({Key? key}) : super(key: key);
+  TimeLineWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +14,15 @@ class TimeLineWidget extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.grey[200],
           border: Border.all(
-            color: Colors.green,
+            color: Colors.black,
           ),
         ),
         child: CustomPaint(
-          painter: CurvePainter(height: 100, width: 700),
-        ),
+            painter: CurvePainter(
+              height: 100,
+              width: 700,
+            ),
+            child: stackBuilder()),
       ),
     );
   }
@@ -24,12 +30,14 @@ class TimeLineWidget extends StatelessWidget {
 
 class CurvePainter extends CustomPainter {
   int delta = 50;
-  int? width;
-  int? height;
+  double width;
+  int height;
+  //double scale;
+  LineContainerSourse lineContainerSourse = LineContainerSourse();
 
-  CurvePainter({@required this.height, required this.width});
+  CurvePainter({required this.height, required this.width});
 
-  void update(int widht, int height) {
+  void update(double widht, int height) {
     this.width = widht;
     this.height = height;
   }
@@ -38,14 +46,25 @@ class CurvePainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     Path path = Path();
     Paint paint = Paint();
+    print('width ${width}');
 
     paint.color = Colors.red;
-    final delta_ = width! / 10;
+    final delta_ = width / 15;
     paint.strokeWidth = 1;
 
-    for (int i = 0; i < 10; i++) {
-      canvas.drawLine(Offset(i * delta_.toDouble(), 0),
-          Offset(i * delta_.toDouble(), 100), paint);
+    ScaleParams scaleParams = lineContainerSourse.getScalePoint(width);
+
+    for (int i = 0; i < scaleParams.countLines; i++) {
+      double x1 = i * scaleParams.delta;
+      double y1 = 0;
+
+      double x2 = i * scaleParams.delta;
+      double y2 = 50;
+
+      var start = Offset(x1, y1);
+      var end = Offset(x2, y2);
+
+      canvas.drawLine(start, end, paint);
     }
     path.close();
   }
@@ -53,6 +72,25 @@ class CurvePainter extends CustomPainter {
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     // TODO: implement shouldRepaint
-    throw UnimplementedError();
+    return true;
   }
+}
+
+Widget stackBuilder() {
+  List<Widget> widgets = [];
+
+  for (int i = 0;
+      i < LineContainerSourse().getScalePoint(700).countLines;
+      i++) {
+    var widget = Positioned(
+      left: i * LineContainerSourse().getScalePoint(700).delta,
+      bottom: 10,
+      child: Text('${i * 5}'),
+    );
+    widgets.add(widget);
+  }
+
+  return Stack(
+    children: widgets,
+  );
 }
